ea57b812e5a46a34a982da725e289a837954a5a0
DenastySS <67180787+DenastySS@users.noreply.github.com>
2022-02-14 21:13:59 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,12 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_taimen.mk \
-    $(LOCAL_DIR)/aosp_taimen_hwasan.mk \
-    $(LOCAL_DIR)/arrow_taimen.mk
+    $(LOCAL_DIR)/xdroid_taimen.mk \
 
 COMMON_LUNCH_CHOICES := \
-    aosp_taimen-userdebug \
-    arrow_taimen-eng \
-    arrow_taimen-user \
-    arrow_taimen-userdebug
+    xdroid_taimen-userdebug \
+    xdroid_taimen-eng \
+    xdroid_taimen-user \
